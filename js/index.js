b6957c3241790db90c33618551c8e84b76fa94e4
taotianran <taotianran@limijiaoyin.com>
2015-11-17 20:09:15 +0800

    add overview json

--- js/index.js
+++ js/index.js
@@ -1,7 +1,107 @@
 /**
  * Created by Administrator on 2015/11/11.
  */
-$(function(){
+
+/**
+ * 1. 详情页综述组件 createReview
+ * 2. 详情页列表组件 createCards
+ */
+var createReview = function(review) {
+    var result = '';
+    result = '<h2>' + review.title + '</h2>' +
+             '<p>' + review.content + '</p>' 
+    return result;
+};
+
+var createCards = function(cards) {
+    var singleCard = [];
+    for (var i = 0; i < cards.length; i++) {
+        singleCard.push(
+            '<li>' +
+                '<div class="hd">' +
+                    '<img class="logo" src="' + cards[i].icon + '" alt=""/>' +
+                    '<h3 class="name">' + cards[i].app + '</h3>' +
+                '</div>' +
+                '<div class="bd">' + 
+                    '<img class="img" src="' + cards[i].image + '" alt=""/>' +
+                    '<h3 class="h3">' + cards[i].title + '</h3>' +
+                    '<p class="p">' + cards[i].desc + '</p>' +
+                '</div>' +
+            '</li>'
+        ); 
+    };
+    return singleCard.join('');
+};
+
+/**
+ *  一览下载组件 
+ */
+var createDownload = function(download) {
+    var result = '<div class="download_desc">' + download.desc + '</div>' +
+                 '<div class="logo">' +
+                    '<img class="logo_image" src="images/logo3.png"/>' +
+                    '<div class="logo_desc">豌豆荚一览下载</div>' +
+                 '</div>' +
+                 '<div class="download_button">下载</div>' +
+                 '<div class="app_image">' +
+                    '<img src="'+ download.image + '"/>' +
+                 '</div>'; 
+    return result;
+};
+
+/**
+ * 顶部导航栏组件
+ */
+var createNavigator = function(nav) {
+    var result = '<a class="btn-back external" href="javascript:;"></a>' +
+                 '<p>' + nav.desc + '</p>' +
+                 '<h1>' + nav.theme + '</h1>' +
+                 '<a class="share" href="javascript:;">分享</a>';
+    return result;
+}
+
+/**
+ * 合作伙伴组件
+ */
+var createPartners = function(partners) {
+    var images = [];
+    partners.forEach(function(partner) {
+        images.push('<img src="' + partner.logo +'"/>'); 
+    });
+
+    var result = '<div class="partners_desc">合作伙伴</div>' +
+                 '<div class="partners_logs">' +
+                    images.join('') +
+                 '</div>';
+    return result;
+}
+
+$(function() {
+    // 摘要页数据配置
+    $.get('/api/overview', function(data) {
+        var details = data.data.details;
+
+        details.forEach(function(detail, index) {
+            var navigation = detail.navigation;
+            var review = detail.review;
+            var cards = detail.cards; 
+            var download = detail.download;
+
+            var number = index + 2;
+            $('.page' + number + ' .head').append(createReview(review));
+            $('.page' + number + ' .list2').append(createCards(cards));
+            $('.page' + number + ' .bar').append(createNavigator(navigation));
+            $('.page' + number + ' .download').append(createDownload(download));
+        });
+    });
+
+    //合作伙伴配置
+    $.get('/api/partners', function(data) {
+        var partners = data.data.partners; 
+
+        $('.partners').append(createPartners(partners));
+    });
+
     //查看详情
     $(document).on('click', '.link', function (e){
         var index = $(this).attr('data-type');
@@ -57,8 +157,7 @@ $(function(){
         },600);
         $('.page-detail .content').scrollTop(0);
     });
-
-    function pageMove(tw){
+function pageMove(tw){
         var lastPage = ".page"+last, nowPage = ".page"+now;
 
         switch(tw){
@@ -104,5 +203,3 @@ $(function(){
 
     $.init();
 });
-
-
