bcc1a6cadefb1618a94d1d6b3dd83e18be917ace
taotianran <taotianran@limijiaoyin.com>
2015-11-18 15:24:09 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -1,6 +1,41 @@
-/**
- * Created by Administrator on 2015/11/11.
- */
+const towards = {
+    left: 1,
+    right: 2
+}
+
+function pageMove(tw, now, last) {
+    var lastPage = '.page' + last;
+    var nowPage = ".page" + now;
+    switch(tw) {
+        case towards.left:
+            outClass = 'page-moveToLeft';
+            inClass = 'page-moveFromRight';
+            break;
+
+        case towards.right:
+            outClass = 'page-moveToRight';
+            inClass = 'page-moveFromLeft';
+            break;
+    }
+
+    isAnimating = true;
+
+    $(nowPage).show();
+
+    $(lastPage).addClass(outClass);
+    $(nowPage).addClass(inClass);
+
+    setTimeout(function() {
+        $(lastPage).removeClass(outClass);
+        $(lastPage).hide();
+
+        $(nowPage).removeClass(inClass);
+    }, 600);
+
+    setTimeout(function() {
+        isAnimating = false;
+    }, 2000);
+}
 
 /**
  * 封面组件
@@ -168,6 +203,15 @@ $(function() {
         $('.index .content').append(createCover(cover));
     });
 
+    // 长文章数据配置
+    $.get('/api/article', function(data) {
+        var navigation = data.data.navigation;
+        var download = data.data.download;
+
+        $('.page1 .bar').append(createNavigator(navigation));
+        $('.page1 .download').append(createDownload(download));
+    });
+
     // 排行榜数据配置
     $.get('/api/rank', function(data) {
         var navigation = data.data.navigation;
@@ -217,22 +261,18 @@ $(function() {
     });
 
     //变量
-    var now = 1, last = 0;
-    const towards = { left:1, right:2 };
     var isAnimating = false;
 
     //向左
-    $('.page-detail').on('swipeLeft', function () {
+    $('.page-detail').on('swipeLeft', function() {
         if (isAnimating) return;
-        now = $(this).index();
-        last = now;
-        console.log('now1:' + now);
-        console.log('last1:' + last);
+        var now = $(this).index();
+        var last = now;
 
-        if(last != 7){
-            now = last+1; pageMove(towards.left);
-            console.log('now:' + now);
-        }else{
+        if (last !== 7) {
+            now = last + 1;
+            pageMove(towards.left, now, last);
+        } else {
             $.alert('已经到最后一页了');
         }
     });
@@ -240,10 +280,12 @@ $(function() {
     //向右
     $('.page-detail').on('swipeRight', function () {
         if (isAnimating) return;
-        last = now;
-        if(last != 1){
-            now = last-1; pageMove(towards.right);
-        }else{
+        var now = $(this).index();
+        var last = now;
+        if(last !== 1){
+            now = last - 1;
+            pageMove(towards.right, now, last);
+        } else {
             $.alert('已经到第一页了');
         }
     });
@@ -256,42 +298,9 @@ $(function() {
         setTimeout(function(){
             parent.removeClass('page-moveToBottom');
             parent.hide();
-        },600);
+        }, 600);
         $('.page-detail .content').scrollTop(0);
     });
-function pageMove(tw){
-        var lastPage = ".page"+last, nowPage = ".page"+now;
-
-        switch(tw){
-            case towards.left:
-                outClass = 'page-moveToLeft';
-                inClass = 'page-moveFromRight';
-                break;
-
-            case towards.right:
-                outClass = 'page-moveToRight';
-                inClass = 'page-moveFromLeft';
-                break;
-        }
-
-        isAnimating = true;
-
-        $(nowPage).show();
-
-        $(lastPage).addClass(outClass);
-        $(nowPage).addClass(inClass);
-
-        setTimeout(function(){
-            $(lastPage).removeClass(outClass);
-            $(lastPage).hide();
-
-            $(nowPage).removeClass(inClass);
-        },600);
-
-        setTimeout(function(){
-            isAnimating = false;
-        },2000);
-    }
 
     //监听滚动
     /*$('.content').on('scroll',function(){
